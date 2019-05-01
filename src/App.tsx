dbc96f34240c75a6c45bd7c88ff143bed1e16a93
fischi <fischi.ftw@gmail.com>
2019-05-01 15:11:12 +0200

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#0277bd",
+      light: "#58a5f0",
+      dark: "#004c8c",
+      contrastText: "#ffffff"
+    },
+    secondary: {
+      main: "#d32f2f",
+      light: "#ff6659",
+      dark: "#9a0007",
+      contrastText: "#ffffff"
+    }
+  }
+});
 
 const App: React.FC = () => {
   return (
-    <h1>Heading</h1>
+    <MuiThemeProvider theme={theme}>
+      <Typography>Text</Typography>
+    </MuiThemeProvider>
   );
 }
 
