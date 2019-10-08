9f4ed67bab9925d57deaf421ffc4feddd1349392
Lukas <fischi.ftw@gmail.com>
2019-10-08 21:45:28 +0200

    removed hocs from TypingCanvas

--- src/shared/typing/TypingCanvas.tsx
+++ src/shared/typing/TypingCanvas.tsx
@@ -1,28 +1,21 @@
-import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
 import React, { FC, useEffect } from 'react';
-import { RouteComponentProps, withRouter } from 'react-router';
 import useResetFocus from '../hooks/useResetFocus';
 import useGermanKeysState from '../keyboard/useGermanKeysState';
 import { GameResultType } from './GameResultType';
 import { ttKeyPressed } from './typeTracking';
 import TypingRoot from './TypingRoot';
+import { useHistory } from 'react-router';
 
 const testText = "Far... far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic lif.";
 // const testText = "asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf";
 // const testText = "asdf asdf asdf asdf asdf";
 
-const styles = () => createStyles({
-    container: {
-    }
-});
-
 type Props = {
-}
-& WithStyles<typeof styles>
-& RouteComponentProps;
+};
 
 const TypingCanvas : FC<Props> = (props) => {    
-    const { classes } = props;
+
+    const history = useHistory();
 
     useGermanKeysState({onKeyDown: key => {
         ttKeyPressed(key);
@@ -34,7 +27,7 @@ const TypingCanvas : FC<Props> = (props) => {
 
         const gameComplete = (result : GameResultType) : void=> {
             console.log(result);
-            props.history.push("about");
+            history.push("about");
         };
 
         const typing = new TypingRoot({
@@ -47,11 +40,11 @@ const TypingCanvas : FC<Props> = (props) => {
             typing.destroy();
         });
         
-    }, [props.history])
+    }, [history]);
 
     return (<>
-        <div className={classes.container} id="typing-area-container"/>
+        <div id="typing-area-container"/>
     </>);
 }
 
-export default withRouter(withStyles(styles)(TypingCanvas));
+export default TypingCanvas;
