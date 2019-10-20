daed357e537bbd0b4f0c234a477254394645f1bb
Lukas <fischi.ftw@gmail.com>
2019-10-20 10:28:49 +0200

    calculating xp, level and xp for the next level

--- src/components/general/UserInfoArea.tsx
+++ src/components/general/UserInfoArea.tsx
@@ -1,17 +1,37 @@
-import React, { FC } from "react";
+import React, { FC, useMemo } from "react";
 import AccountInformation from "../interface/AccountInformation";
 import { useUserInfoState } from "../context/UserInfoProvider";
+
+const initialXP = 150;
 
 const UserInfoArea: FC<{}> = () => {
 
     const userInfo = useUserInfoState();
 
+    const lvlInfo = useMemo(() => {
+        var level = 0;
+        
+        var xp = userInfo.xp;
+        console.log(xp);
+
+        while(xp - (initialXP * (level + 1)) >= 0) {
+            xp -= initialXP * (level + 1);
+            level++;
+        }
+
+        const xpForNextLevel = initialXP * (level + 1);
+    
+        return {xp: xp, level: level, xpForNextLevel: xpForNextLevel};
+    }, [userInfo.xp]);
+
+    console.table(lvlInfo);
+
     return (
         <AccountInformation 
-            curXP={userInfo.xp}
+            curXP={lvlInfo.xp}
             gems={userInfo.gems}
-            lvl={125}
-            nextLvlXP={8000}
+            lvl={lvlInfo.level}
+            nextLvlXP={lvlInfo.xpForNextLevel}
             username={userInfo.username}
         />
     );
