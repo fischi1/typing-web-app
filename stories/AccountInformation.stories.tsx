9760c95f52e9d0fb62c08719f82bcba9460122e7
Lukas <fischi.ftw@gmail.com>
2019-10-24 23:41:49 +0200

    AccountInformation story

---
+++ stories/AccountInformation.stories.tsx
@@ -0,0 +1,15 @@
+import { number, text } from '@storybook/addon-knobs';
+import { storiesOf } from "@storybook/react";
+import React from "react";
+import AccountInformation from "../src/components/interface/AccountInformation";
+
+storiesOf("AccountInformation", module)
+.add("example", () => (
+    <AccountInformation 
+        username={text("username", "username")}
+        lvl={number("lvl", 0)}
+        curXP={number("curXP", 333)}
+        nextLvlXP={number("nextLvlXP", 1000)}
+        gems={number("gems", 5021)}
+    />
+));
